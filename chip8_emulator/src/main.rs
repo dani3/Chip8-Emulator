4167c281d0318edb426ec7717bd14d3cec07a992
Daniel Mancebo <dani.mancebo_3@hotmail.com>
2019-08-09 19:46:51 +0200

    Fixed syntax errors

--- chip8_emulator/src/main.rs
+++ chip8_emulator/src/main.rs
@@ -11,16 +11,16 @@ fn main() {
     let program_area: (u16, u16) = (0x200, 0xFff);
 
     // Stack
-    let stack: [u16, 16] = [0xff, 16];
+    let stack: [u16; 16] = [0xff; 16];
     let mut sp: u16;
 
     // Keypad
-    let keypad: [u8, 16] = [0x00, 16];
+    let keypad: [u8; 16] = [0x00; 16];
 
     // Graphics
-    let gfx: [u8, 64 * 32] = [0x00; 64 * 32];
+    let gfx: [u8; 64 * 32] = [0x00; 64 * 32];
     // CPU registers
-    let V: [u8; 16] = [0xff, 16];
+    let V: [u8; 16] = [0xff; 16];
     // Index register
     let mut I: u16;
     // Program counter
