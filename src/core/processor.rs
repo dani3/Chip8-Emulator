bd47bfd4190176d96810d8cb8336f5c7925aae4d
Daniel Mancebo <dani.mancebo_3@hotmail.com>
2019-08-13 18:44:44 +0200

    exec_sub_vx_vy opcode implemented

--- src/core/processor.rs
+++ src/core/processor.rs
@@ -146,6 +146,7 @@ impl Processor {
                 (0x8,_,_,0x2)     => self.exec_and_vx_vy(x, y),
                 (0x8,_,_,0x3)     => self.exec_xor_vx_vy(x, y),
                 (0x8,_,_,0x4)     => self.exec_add_vx_vy(x, y),
+                (0x8,_,_,0x5)     => self.exec_sub_vx_vy(x, y),
                 (_,_,_,_)         => ()
             }
         }
@@ -356,6 +357,24 @@ impl Processor {
         self.increment_pc();
 
         println!("ADD V{:x?} -> V{:x?} (VF = {:x?})", y, x, self.v[0xf]);
+    }
+
+    /// __8xy5 - SUB Vx, Vy__
+    /// Set Vx = Vx - Vy. Set VF = NOT borrow.
+    ///
+    /// If Vx > Vy, then VF is set to 1, otherwise 0. Then Vy is subtracted
+    /// from Vx, and the results stored in Vx.
+    fn exec_sub_vx_vy(&mut self, x: u8, y: u8) {
+        if self.v[y as usize] < self.v[x as usize] {
+            self.v[0xf] = 1;
+        } else {
+            self.v[0xf] = 0;
+            self.v[x as usize] -= self.v[y as usize];
+        }
+
+        self.increment_pc();
+
+        println!("SUB V{:x?} -> V{:x?} (VF = {:x?})", y, x, self.v[0xf]);
     }
 
     /// Return the opcode currently pointed from the program counter.
